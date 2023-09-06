5f8869c887472ecd1c54df2cea393f8054618194
Sara Palacio <saritapz01@hotmail.com>
2023-09-05 20:44:45 -0500

    Agregando proyectos y estilo

--- src/components/CardProjects.jsx
+++ src/components/CardProjects.jsx
@@ -1,28 +1,95 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { BiLogoReact, BiLogoJavascript, BiSolidFileCss, BiLogoHtml5 } from "react-icons/bi";
 
 const CardProjects = () => {
     return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <div className='cont-projects'>
+      <h1>Proyectos realizados</h1>
+        <Card style={{ width: '20rem' }}>
+          <Card.Img style={{ height: '18rem' }} variant="top" src="https://res.cloudinary.com/dez9y1otk/image/upload/v1693950213/Portfolio/Imagen1_ildwv2.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            <Card.Title>Mood Master</Card.Title>
+            <Card.Text className='logo-lenguaje'>
+              Tecnologías utilizadas: <span><BiLogoReact /> <BiLogoJavascript /> <BiSolidFileCss/> <BiLogoHtml5 /> </span>
             </Card.Text>
           </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
           <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Link href="#">Repositorio Github</Card.Link>
+            <Card.Link href="#">Funcionamiento</Card.Link>
           </Card.Body>
         </Card>
+
+        <Card style={{ width: '20rem' }}>
+          <Card.Img style={{ height: '18rem' }} variant="top" src="https://res.cloudinary.com/dez9y1otk/image/upload/v1693951275/Portfolio/img-proyecto9_nftkvn.png" />
+          <Card.Body>
+            <Card.Title>Menú App</Card.Title>
+            <Card.Text className='logo-lenguaje'>
+              Tecnologías utilizadas: <span><BiLogoReact /> <BiLogoJavascript /> <BiSolidFileCss/> <BiLogoHtml5 /> </span>
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Link href="https://github.com/sarapalac10/Alkemy-MenuApp">Repositorio Github</Card.Link>
+            <Card.Link href="https://sarapalac10.github.io/Alkemy-MenuApp/">Funcionamiento</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '20rem' }}>
+          <Card.Img style={{ height: '18rem' }} variant="top" src="https://res.cloudinary.com/dez9y1otk/image/upload/v1693964327/Portfolio/colorgame_r6wlog.png" />
+          <Card.Body>
+            <Card.Title>Color Game</Card.Title>
+            <Card.Text className='logo-lenguaje'>
+              Tecnologías utilizadas: <span><BiLogoJavascript /> <BiSolidFileCss/> <BiLogoHtml5 /> </span>
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Link href="https://github.com/sarapalac10/ColourGame">Repositorio Github</Card.Link>
+            <Card.Link href="https://sarapalac10.github.io/ColourGame/">Funcionamiento</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '20rem' }}>
+          <Card.Img style={{ height: '18rem' }} variant="top" src="https://res.cloudinary.com/dez9y1otk/image/upload/v1693951290/Portfolio/img-proyecto0_cynbnd.png" />
+          <Card.Body>
+            <Card.Title>Calculadora</Card.Title>
+            <Card.Text className='logo-lenguaje'>
+              Tecnologías utilizadas: <span><BiLogoJavascript /> <BiSolidFileCss/> <BiLogoHtml5 /> </span>
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Link href="https://github.com/sarapalac10/Reto2-Sprint1">Repositorio Github</Card.Link>
+            <Card.Link href="https://sarapalac10.github.io/Reto2-Sprint1/">Funcionamiento</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '20rem' }}>
+          <Card.Img style={{ height: '18rem' }} variant="top" src="https://res.cloudinary.com/dez9y1otk/image/upload/v1693964327/Portfolio/drumkit_belckp.png" />
+          <Card.Body>
+            <Card.Title>Batería App</Card.Title>
+            <Card.Text className='logo-lenguaje'>
+              Tecnologías utilizadas: <span><BiLogoJavascript /> <BiSolidFileCss/> <BiLogoHtml5 /> </span>
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Link href="https://github.com/sarapalac10/DrumKit">Repositorio Github</Card.Link>
+            <Card.Link href="https://sarapalac10.github.io/DrumKit/">Funcionamiento</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '20rem' }}>
+          <Card.Img style={{ height: '18rem' }} variant="top" src="https://res.cloudinary.com/dez9y1otk/image/upload/v1693961398/Portfolio/img-proyecto4_cb0h88.png" />
+          <Card.Body>
+            <Card.Title>Conversor de moneda</Card.Title>
+            <Card.Text className='logo-lenguaje'>
+              Tecnologías utilizadas: <span> <BiLogoJavascript /> <BiSolidFileCss/> <BiLogoHtml5 /> </span>
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Link href="https://github.com/sarapalac10/Reto1-AG">Repositorio Github</Card.Link>
+            <Card.Link href="https://sarapalac10.github.io/Reto1-AG/">Funcionamiento</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
       );
 }
 
--- src/components/PreviewProjects.jsx
+++ src/components/PreviewProjects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import '../style/ProjectSkills.css';
+import '../style/FullProjectStyles.css'
 
 const PreviewProjects = () => {
   return (
